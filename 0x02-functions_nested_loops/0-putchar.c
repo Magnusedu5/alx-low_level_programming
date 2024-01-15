4ef41b15e1b0fd031ce31661738fe6a067fdd2bc
Magnus Edu <magnusedu5@gmail.com>
2024-01-15 14:34:37 -0800

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
- * main.h - header file
- *
- * main - Entry point
- *
- * Return: 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
